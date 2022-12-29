007cc3909694fe903f581b26d4adfadf5d7ca7b8
sawa1113 <63143529+sawa1113@users.noreply.github.com>
2022-12-30 01:27:39 +0900

    双方のモデルを関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  mount_uploader :profile_image_id, ImageUploader
+  has_many :cats, dependent: :destroy
 end
