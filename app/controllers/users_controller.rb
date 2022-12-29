3e7f1e8b1e5b7e3c1bcb448eefd3847c91953f80
sawa1113 <63143529+sawa1113@users.noreply.github.com>
2022-12-30 01:28:41 +0900

    edit,updateアクションの修正とストロングパラメーターの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,10 +9,22 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to user_path(current_user), alert: "不正なアクセスです。"
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "ユーザー情報を更新しました。"
+    else
+      render :edit
+    end
   end
 
   private
   def user_params
-    params.require(:user).permit(:username, :email, :profile, :profile_image)
+    params.require(:user).permit(:username, :email, :profile, :profile_image_id)
   end
 end
