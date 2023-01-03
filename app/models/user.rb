9b9766ba15b9072917fc051c2ce2c1157c72505b
sawa1113 <63143529+sawa1113@users.noreply.github.com>
2023-01-04 02:21:19 +0900

    いいね用の中間テーブル作成とアソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   mount_uploader :profile_image_id, ImageUploader
   has_many :cats, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   validates :username, presence: true
 end
